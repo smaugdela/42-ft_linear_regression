da69cc6538206139b89daa14a5883747be3358c5
smagdela <smagdela@student.42.fr>
2023-09-07 18:34:38 +0200

    Running but not producing expected result. Should now look over to turn algebraic equations

--- training.py
+++ training.py
@@ -1,5 +1,6 @@
 import pandas
 import matplotlib.pyplot as plt
+import numpy as np
 
 def	get_estimate(mileage, theta0, theta1):
 	"""
@@ -13,7 +14,7 @@ def	cost_function():
 	"""
 	pass
 
-def gradient_descent(learning_rate: float, mileage_list, price_list, max_iter, epsilon):
+def gradient_descent(learning_rate, mileage_list, price_list, max_iter, epsilon):
 	"""
 	Main logic for learning algorithm given learning_rate (alpha) and returns a tuple of the computed theta0 and theta1.
 	max_iter is the maximum number of iteration that will be run, to avoid infinite loop in case of divergence.
@@ -25,8 +26,8 @@ def gradient_descent(learning_rate: float, mileage_list, price_list, max_iter, e
 	m = len(mileage_list)
 	coef = learning_rate / m
 
-	i = -1
-	while (++i < max_iter):
+	i = 0
+	while (i < max_iter):
 
 		error = 0
 		for (mileage, price) in zip(mileage_list, price_list):
@@ -42,13 +43,18 @@ def gradient_descent(learning_rate: float, mileage_list, price_list, max_iter, e
 
 		theta0 = theta0 - temp0
 		theta1 = theta1 - temp1
+		# theta0 = temp0
+		# theta1 = temp1
 
-		if (temp0 < epsilon and temp1 < epsilon):
-			break
+		# if (abs(temp0) < epsilon and abs(temp1) < epsilon):
+		# 	break
+		
+		i += 1
 
-	return(theta0, theta1)
+	return (theta0, theta1)
 
 if __name__ == "__main__":
+
 	try:
 		data = pandas.read_csv("data.csv")
 		if (len(data['price']) != len(data['km'])):
@@ -60,16 +66,25 @@ if __name__ == "__main__":
 
 	print("Training...")
 
-	new_parameters = gradient_descent(0.01, data['km'], data['price'], 10000000, 0.00001)
+	(theta0, theta1) = gradient_descent(1e-10, data['km'], data['price'], 10000, 0.1)
 
-	fig = plt.subplots()
-	fig.scatter(data['km'], data['price'])
-	fig.plot()
+	print(theta0, theta1)
+
+	min = min(data["km"])
+	max = max(data["km"])
+	x = np.linspace(min, max, 2)
+	y = theta0 + (theta1 * x)
+
+	plt.plot(data["km"], data["price"], 'bx')
+	plt.plot(x, y, "r")
+	plt.xlabel("mileage (km)")
+	plt.ylabel("price")
+	plt.show()
 
 	try:
 		data = {
 			'parameter': ['theta0', 'theta1'],
-			'value': [new_parameters[0], new_parameters[1]]
+			'value': [theta0, theta1]
 		}
 		df = pandas.DataFrame(data)
 		df.set_index("parameter", inplace=True)
